1d58f5e2c8d4a28d55cc7230ef6d547b7c709f8b
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2024-07-29 20:04:58 -0500

    add: BudgetForm component + form validation

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,45 @@
+import { useMemo, useState } from "react";
+
+const BudgetForm = () => {
+  // Este componente es el formulario donde el usuario define su presupuesto
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //Funcion para atrapar el valor del presupuesto
+    setBudget(e.target.valueAsNumber);
+  };
+
+  const isValid = useMemo(() => {
+    // Esta funcion valida que budget no sea Nan y que sea mayor que 0
+    // usamos memo para un mejor rendimiento
+    return isNaN(budget) || budget <= 0;
+  }, [budget]);
+
+  return (
+    <form action="" className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-4xl text-blue-600 font-bold text-center"
+        >
+          Definir Presupuesto
+        </label>
+        <input
+          type="number"
+          className="w-full bg-white border border-gray-200 p-2 "
+          placeholder="Define tu presupuesto"
+          name="budget"
+          value={budget}
+          onChange={handleChange}
+        />
+      </div>
+      <input
+        type="submit"
+        value={"Definir Presupuesto"}
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40" // Si el boton es disable se aplica un ioacity de 40
+        disabled={isValid} // si es true se le aplica desable al boton
+      />
+    </form>
+  );
+};
+export default BudgetForm;
